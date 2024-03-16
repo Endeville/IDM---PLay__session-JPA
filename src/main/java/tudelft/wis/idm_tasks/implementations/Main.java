235a86417553484ea3850614135a70edc1873efd
Victor Popdonchev <V.I.Popdonchev@student.tudelft.nl>
2024-03-16 15:22:21 +0100

    javax persistence Entity manager

--- src/main/java/tudelft/wis/idm_tasks/implementations/Main.java
+++ src/main/java/tudelft/wis/idm_tasks/implementations/Main.java
@@ -2,11 +2,13 @@ package tudelft.wis.idm_tasks.implementations;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Manager m = new Manager();
-        Connection c = m.getConnection();
-
+             var entityMng =Persistence.createEntityManagerFactory("idm_jpa");
+             var em=entityMng.createEntityManager();
     }
 }
